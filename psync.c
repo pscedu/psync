b517ff85e35adfd79dab56549086ec461de05e81
Jared Yanovich <slovichon@gmail.com>
2015-03-04 19:51:33 -0500

    fix iostat math flub

--- psync.c
+++ psync.c
@@ -877,7 +877,8 @@ dispthr_main(struct psc_thread *thr)
 
 	PFL_GETTIMESPEC(&ts);
 	timespecsub(&ts, &start, &d);
-	psc_fmt_human(ratebuf, iostats->opst_last);
+	psc_fmt_human(ratebuf, iostats->opst_lifetime /
+	    (d.tv_sec + d.tv_nsec * 1e-9));
 	psc_fmt_human(totalbuf, psc_atomic64_read(&nbytes_total));
 
 	printf("elapsed %02ld:%02ld:%02ld.%02d  %s total  avg %7s/s%s\n",
